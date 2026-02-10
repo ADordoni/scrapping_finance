beac891cdd601222f1725464160382c3f415fd9d
Agustín Dordoni <99514125+ADordoni@users.noreply.github.com>
2026-02-10 00:34:53 -0300

    Add files via upload

---
+++ scrapping/execute.py
@@ -0,0 +1,45 @@
+import funciones as sa
+import yfinance as yf
+import json
+import contextlib
+import io
+import os
+
+sa.exhanges()
+sa.diccionario()
+sa.get_tinker()
+sa.shuffle()
+
+f = open(f'landing/metadata/catalogo.json','r')
+catalogo=json.loads(f.read())
+
+for ct in catalogo:
+    # We set a break to avoid a complete but heavy load
+    if ct['country'] == 'Austria':
+        break
+    os.makedirs(f'landing/historical/{ct['country']}',exist_ok=True)
+    os.makedirs(f'landing/exhanges/{ct['country']}',exist_ok=True)
+    sf=ct['sufix']
+    failed=0
+    i=0
+    
+    # We set a limit to avoid a complete but heavy load
+    while i < 20:
+        buffer=io.StringIO()
+        ac=ct['symbols'][i]
+
+        with contextlib.redirect_stdout(buffer),contextlib.redirect_stderr(buffer):
+
+            tc = yf.download(ac+sf,start='1900-01-01')
+            ct['symbols'][i]=[ac,tc.info]
+
+            historial=tc.to_csv(path_or_buf=f'landing/historical/{ct['country']}/{ac}.csv',sep=';',index=True)
+
+        msj=buffer.getvalue()
+        if 'Failed download' in msj:
+            if failed == 0:
+                print('Failed download')
+            failed=failed+1
+            print(ac)
+
+        i=i+1
